438d1160835900249ba41a991f1d89944aa3a09f
David Lemay <davidjlemay@gmail.com>
2021-09-01 18:22:43 -0400

    Adding drag and drop

--- source/desktop.py
+++ source/desktop.py
@@ -1,29 +1,31 @@
 from source.helpers import *
 from source.components import *
-import tkinter as tk
+from tkinter import *
 from tkinter.dnd import DndHandler
 
-window = tk.Tk()
-window.title("S*C*R*A*M*B*L*E")
-window.geometry('500x500')
-window.config(bg='#F2B33D')
-window.columnconfigure(0, weight=1)
-window.rowconfigure(0, weight=1)
+window = Tk()
+#window.title("S*C*R*A*M*B*L*E")
+#window.geometry('500x500')
+#window.config(bg='#F2B33D')
+#window.columnconfigure(0, weight=1)
+#window.rowconfigure(0, weight=1)
 
-root = tk.LabelFrame(window)
+root = LabelFrame(window)
 root.grid(column=0, row=1, sticky="nsew")
 root.columnconfigure(0, weight=1)
 root.rowconfigure(3, weight=1)
 root.pack(expand=True)
 
+dnd_manager = DndManager(root)
+
 name = 'david'
 username = 'azeroth'
 password = 'qwerty'
 user = User(name, username, password)
-computer = Computer('commodore', 'mr_computer', '10101010')
+computer = User('commodore', 'mr_computer', '10101010')
 game = Game([user, computer])
 
-board = tk.Frame(root)
+board = Frame(dnd_manager.canvas)
 board.grid(column=0, row=0, sticky="nsew")
 board.columnconfigure(0, weight=1)
 board.rowconfigure(0, weight=1)
@@ -31,22 +33,20 @@ board.rowconfigure(0, weight=1)
 for i, row in enumerate(game.board.grid):
     for j, column in enumerate(row):
         tile = game.board.grid[i][j].tile
-        L = tk.Frame(board, width=36, height=36, border=1, relief='sunken', bg='beige')
-        L.grid(row=i, column=j, sticky='nsew')
+        L = Frame(board, width=36, height=36, border=1, relief='sunken', bg='beige')
+        L.grid(row=i, column=j+1, sticky='nsew') #keep first column free for player labels
  #       L.bind('<Button-1>', lambda e, i=i, j=j: on_button_release(i, j, e, x))
 
 game.players[0].add_tiles(game.take_tiles(7))
-#dnd = DragManager()
 
 for n, player in enumerate(game.players):
-    rack = tk.LabelFrame(board, text=f'{player.name}', width=250, height=25, bg='white')
+    rack = LabelFrame(board, text=f'{player.username}', width=250, height=25, bg='white')
     rack.grid(row=len(game.board.grid)+n, column=0, sticky="ws")
     for x, tile in enumerate(player.rack):
-        T = tk.LabelFrame(rack, width=35, height=35, border=1, relief='raised', text=tile.letter, bg='brown')
-        T.grid(row=0, column=x, sticky='nsew')
-        T.bind("<Button-1>", dnd.dnd_start)
-#       dnd.make_draggable(T)
-#       T = Icon(tile.letter)
-#       T.attach(root)
+#        T = Label(rack, width=35, height=35, border=1, relief='raised', text=tile.letter, bg='red')
+#        T.grid(row=0, column=x, sticky='nsew')
+#        T.bind("<Button-1>", dnd.dnd_start)
+        D = Draggable(tile.letter)
+        D.attach(dnd_manager.canvas)
 
-root.mainloop()
+window.mainloop()
--- source/helpers.py
+++ source/helpers.py
@@ -1,26 +1,100 @@
-from tkinter import dnd
+from tkinter import Canvas, dnd, Label, Toplevel
 
-class DragManager():
-    def make_draggable(self, widget):
-        widget.bind("<Button-1>", self.on_start)
-        widget.bind("<B1-Motion>", self.on_drag)
-        widget.bind("<ButtonRelease-1>", self.on_drop)
-        widget.configure(cursor="hand1")
 
-    def on_start(self, event):
-        widget = event.widget
-        widget._drag_start_x = event.x
-        widget._drag_start_y = event.y
+class Draggable:
 
-    def on_drag(self, event):
-        widget = event.widget
-        x = widget.winfo_x() + event.x
-        y = widget.winfo_y() + event.y
-        widget.place(x=x, y=y)
+    def __init__(self, name):
+        self.name = name
+        self.canvas = self.label = self.id = None
 
-    def on_drop(self, event):
-#       Player.place_tile(i, j, tile)
+    def attach(self, canvas, x=10, y=10):
+        if canvas is self.canvas:
+            self.canvas.coords(self.id, x, y)
+            return
+        if self.canvas:
+            self.detach()
+        if not canvas:
+            return
+        label = Label(canvas, width=4, height=2, text=self.name, borderwidth=2, relief="raised", bg="brown")
+        id = canvas.create_window(x, y, window=label, anchor="nw")
+        self.canvas = canvas
+        self.label = label
+        self.id = id
+        label.bind("<ButtonPress>", self.press)
+
+    def detach(self):
+        canvas = self.canvas
+        if not canvas:
+            return
+        id = self.id
+        label = self.label
+        self.canvas = self.label = self.id = None
+        canvas.delete(id)
+        label.destroy()
+
+    def press(self, event):
+        if dnd.dnd_start(self, event):
+            # where the pointer is relative to the label widget:
+            self.x_off = event.x
+            self.y_off = event.y
+            # where the widget is relative to the canvas:
+            self.x_orig, self.y_orig = self.canvas.coords(self.id)
+
+    def move(self, event):
+        x, y = self.where(self.canvas, event)
+        self.canvas.coords(self.id, x, y)
+
+    def putback(self):
+        self.canvas.coords(self.id, self.x_orig, self.y_orig)
+
+    def where(self, canvas, event):
+        # where the corner of the canvas is relative to the screen:
+        x_org = canvas.winfo_rootx()
+        y_org = canvas.winfo_rooty()
+        # where the pointer is relative to the canvas widget:
+        x = event.x_root - x_org
+        y = event.y_root - y_org
+        # compensate for initial pointer offset
+        return x - self.x_off, y - self.y_off
+
+    def dnd_end(self, target, event):
         pass
+
+
+class DndManager:
+
+    def __init__(self, root):
+        self.top = Toplevel(root)
+        self.canvas = Canvas(self.top, width=500, height=500)
+        self.canvas.pack(fill="x", expand=1)
+        self.canvas.dnd_accept = self.dnd_accept
+        self.dnd_id = None
+
+    def dnd_accept(self, source, event):
+        return self
+
+    def dnd_enter(self, source, event):
+        self.canvas.focus_set() # Show highlight border
+        x, y = source.where(self.canvas, event)
+        x1, y1, x2, y2 = source.canvas.bbox(source.id)
+        dx, dy = x2-x1, y2-y1
+        self.dnd_id = self.canvas.create_rectangle(x, y, x + dx, y + dy)
+        self.dnd_motion(source, event)
+
+    def dnd_motion(self, source, event):
+        x, y = source.where(self.canvas, event)
+        x1, y1, x2, y2 = self.canvas.bbox(self.dnd_id)
+        self.canvas.move(self.dnd_id, x - x1, y - y1)
+
+    def dnd_leave(self, source, event):
+        self.top.focus_set() # Hide highlight border
+        self.canvas.delete(self.dnd_id)
+        self.dnd_id = None
+
+    def dnd_commit(self, source, event):
+        self.dnd_leave(source, event)
+        x, y = source.where(self.canvas, event)
+        source.attach(self.canvas, x, y)
 
 
 def press(self, event):
